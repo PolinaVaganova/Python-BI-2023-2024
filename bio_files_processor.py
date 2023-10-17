3a8dd96d83ab812c252b25ecf1cfe462c4410514
Polina Vaganova <polvagser1@gmail.com>
2023-10-17 23:57:44 +0300

    feature: Add code for`select_genes_from_gbk_to_fasta` funct

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -31,16 +31,64 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
         fout.write(seq)
 
 
-def select_genes_from_gbk_to_fasta(input_gbk: str, genes: list,  n_before: int = 1, n_after: int = 1,
+def select_genes_from_gbk_to_fasta(input_gbk: str, genes: list, n_before: int = 1, n_after: int = 1,
                                    output_fasta: str = None) -> None:
     """
     Creates FASTA file with neighbour CDSs to given genes from GBK file in fasta_selected_from_gbk directory.
     :param input_gbk: path to GBK file (str)
-    :param genes: genes of interest that are used for searching neighbour CDSs (str)
+    :param genes: genes of interest that are used for searching neighbour CDSs (list)
     :param n_before: number of neighbor CDSs before gene of interest (int)
     :param n_after: number of neighbor CDSs after gene of interest (int)
     :param output_fasta: name of the output fasta file (str)
     :return: None
     """
+    cds_coord_list = []
+    coord_cds_dict = {}
+    genes_names_dict = {}
+    translation_seq = []
+    record_translation = False
+    gene_name = ''
+    coord = ''
+    with open(input_gbk, mode='r') as gbk:
+        for line in gbk:
+            if line.strip().startswith('CDS '):
+                if coord:
+                    coord_cds_dict[coord] = (gene_name, "".join(translation_seq))
+                    translation_seq = []
+                coord = line.split()[1]
+                cds_coord_list.append(coord)
+                record_translation = False
+            elif '/gene' in line:
+                gene_name = line.split('"')[1]
+                genes_names_dict[gene_name] = coord
+            if record_translation:
+                translation_seq.append(line.strip().strip('"'))
+            elif '/translation' in line:
+                record_translation = True
+                translation_seq.append(line.strip().split('"')[1])
+            elif line.strip().startswith('ORIGIN'):
+                record_translation = False
+                coord_cds_dict[coord] = (gene_name, "".join(translation_seq))
 
-    pass
+        cds_of_interest = []
+
+        for gene in genes:
+            gene_coord = genes_names_dict[gene]
+            gene_position = cds_coord_list.index(gene_coord)
+            if len(cds_coord_list[:gene_position]) < n_before:
+                raise ValueError("Too many neighbours CDSs before gene of interest are requested. Change number!")
+            if len(cds_coord_list[gene_position:]) < n_after:
+                raise ValueError("Too many neighbours CDSs after gene of interest are requested. Change number!")
+            cds_of_interest = cds_coord_list[gene_position - n_before:gene_position + n_after + 1]
+
+        os.makedirs('fasta_selected_from_gbk', exist_ok=True)
+
+        if output_fasta is None:
+            output_fasta = f"CDS_selected_from_gbk_{os.path.basename(input_gbk).split('.')[0]}"
+        if not output_fasta.endswith('.fasta'):
+            output_fasta = f'{output_fasta}.fasta'
+
+        with open(os.path.join('fasta_selected_from_gbk', output_fasta), mode='w') as fasta:
+            for cds in cds_of_interest:
+                fasta.write(f'>{cds} gene:{coord_cds_dict[cds][0]}\n{coord_cds_dict[cds][1]}\n')
+        print('FASTA file is ready.')
