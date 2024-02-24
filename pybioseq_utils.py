a83f1cd7cca40933dc3a1a730ecc7d1dcabf5c2d
Polina Vaganova <polvagser1@gmail.com>
2024-02-25 00:54:43 +0300

    Finish HW14: add functions for aminoacid sequences analysis and reformat nucleic acids analysis code

--- pybioseq_utils.py
+++ pybioseq_utils.py
@@ -11,6 +11,13 @@ class NucleotideNotFoundError(ValueError):
     pass
 
 
+class AminoAcidNotFoundError(ValueError):
+    """
+    Error raised when there is incorrect aminoacid in corresponding sequence.
+    """
+    pass
+
+
 class BiologicalSequence(ABC):
     @abstractmethod
     def __len__(self):
@@ -31,22 +38,50 @@ class BiologicalSequence(ABC):
 
 
 class NucleicAcidSequence(BiologicalSequence, ABC):
+    """
+    Represents a nucleic acid sequence.
+
+    Inherits from:
+        BiologicalSequence: An abstract base class defining the interface for biological sequences.
+
+    Attributes:
+        seq (str): The nucleic acid sequence.
+        complement_pairs (dict): A dictionary mapping nucleotides to their complements.
+
+    Methods:
+        __len__(): Returns the length of the nucleic acid sequence.
+        __getitem__(item): Returns the element or slice of the nucleic acid sequence.
+        __str__(): Returns the string representation of the nucleic acid sequence.
+        is_correct(): Checks if the nucleic acid sequence contains valid nucleotides.
+        complement(): Creates a complementary nucleic acid sequence.
+        gc_content(): Calculates the GC content of the sequence.
+    """
+
     def __init__(self, seq: str):
         self.seq = seq
         self.complement_pairs = {}
 
+    def __len__(self):
+        return len(self.seq)
+
+    def __getitem__(self, index):
+        return self.seq[index]
+
+    def __str__(self):
+        return str(self.seq)
+
     def is_correct(self):
         """
-        Check if seq dna/rna or not
+        Check if seq dna/rna valid
         """
         for nucleotide in set(self.seq):
             if nucleotide.upper() not in self.complement_pairs.keys():
                 raise NucleotideNotFoundError(f'Sequence contains invalid nucleotide: {nucleotide}')
-        return None
+        return 'Your sequence is correct.'
 
     def complement(self):
         """
-        Create complementary nucleic seq
+        Create complementary nucleic acid sequence.
         """
         self.is_correct()
         complement_seq = ''
@@ -60,11 +95,24 @@ class NucleicAcidSequence(BiologicalSequence, ABC):
         return resulting_seq
 
     def gc_content(self):
+        """
+        Calculate the GC content of the sequence as fraction (from 0 to 1).
+        """
         self.is_correct()
         return SeqUtils.gc_fraction(self.seq)
 
 
 class RNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
+    """
+    Represents an RNA sequence.
+
+    Inherits from:
+        NucleicAcidSequence: A generic nucleic acid sequence.
+
+    Methods:
+        __init__(): Initializes an RNA sequence.
+    """
+
     def __init__(self, seq):
         super().__init__(seq)
         self.complement_pairs = {'A': 'U',
@@ -74,6 +122,17 @@ class RNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
 
 
 class DNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
+    """
+    Represents a DNA sequence.
+
+    Inherits from:
+        NucleicAcidSequence: A generic nucleic acid sequence.
+
+    Methods:
+        __init__(): Initializes a DNA sequence.
+        transcribe(): Transcribes the DNA sequence into RNA.
+    """
+
     def __init__(self, seq):
         super().__init__(seq)
         self.complement_pairs = {'A': 'T',
@@ -94,28 +153,96 @@ class DNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
 
 
 class AminoAcidSequence(BiologicalSequence, ABC):
-    def __init__(self, seq: str):
-        self.seq = seq
-        self.complement_pairs = {}
+    """
+    Represents an amino acid sequence.
+
+    Inherits from:
+        BiologicalSequence: An abstract base class defining the interface for biological sequences.
+
+    Attributes:
+        seq (str): The amino acid sequence.
+        code (int): The encoding format for the sequence (1 for one-letter and 3 for three-letter).
+
+    Methods:
+        __len__(): Returns the length of the amino acid sequence.
+        __getitem__(index): Returns the amino acid at the specified index.
+        __str__(): Returns the string representation of the amino acid sequence.
+        is_correct(): Checks if the amino acid sequence contains valid amino acids.
+        count_aa(): Counts the occurrences of each amino acid in the sequence.
+    """
+
+    def __init__(self, seq: str, code: int = 1
+                 ):
+        self.code = code
+        self.residues_encoding = {'ALA': 'A',
+                                  'ARG': 'R',
+                                  'ASN': 'N',
+                                  'ASP': 'D',
+                                  'CYS': 'C',
+                                  'GLN': 'Q',
+                                  'GLU': 'E',
+                                  'GLY': 'G',
+                                  'HIS': 'H',
+                                  'ILE': 'I',
+                                  'LEU': 'L',
+                                  'LYS': 'K',
+                                  'MET': 'M',
+                                  'PHE': 'F',
+                                  'PRO': 'P',
+                                  'SER': 'S',
+                                  'THR': 'T',
+                                  'TRP': 'W',
+                                  'TYR': 'Y',
+                                  'VAL': 'V'
+                                  }
+
+        if self.code == 1:
+            self.seq = [seq[val:val + 1] for val in range(0, len(seq), 1)]
+            self.residues_names = self.residues_encoding.values()
+        elif self.code == 3:
+            self.seq = [seq[val:val + 3] for val in range(0, len(seq), 3)]
+            self.residues_names = self.residues_encoding.keys()
+        else:
+            raise ValueError(f'Invalid specified encoding: {self.code}. Available values: 1, 3.')
+
+    def __len__(self):
+        return len(self.seq)
+
+    def __getitem__(self, index):
+        return self.seq[index]
+
+    def __str__(self):
+        return ''.join(self.seq)
 
     def is_correct(self):
         """
-        Check if seq dna/rna or not
+        Check if protein seq valid or not
         """
-        for nucleotide in set(self.seq):
-            if nucleotide.upper() not in self.complement_pairs.keys():
-                raise NucleotideNotFoundError(f'Sequence contains invalid nucleotide: {nucleotide}')
-        return None
+        for residue in set(self.seq):
+            if residue.upper() not in self.residues_names:
+                raise AminoAcidNotFoundError(f'Sequence contains invalid aminoacid: {residue}')
+        return 'Your sequence is correct.'
+
+    def count_aa(self):
+        """
+        Count entry of each residue type in your seq. Get description of amino acid composition in dict format.
+        :return: each residue type and its amount in current seq (dict)
+        """
+        residue_count = {}
+        for residue in set(self.seq):
+            residue_entry = self.seq.count(residue)
+            residue_count[residue] = residue_entry
+        return residue_count
 
 
-def run_fastaq_filtering(input_path: str, output_filename: str = None, gc_bounds: Union[tuple, float, int] = (0, 100),
-                         length_bounds: Union[tuple, float] = (0, 2 ** 32),
-                         quality_threshold: int = 0) -> None:
+def run_fastq_filtering(input_path: str, output_filename: str = None, gc_bounds: Union[tuple, float, int] = (0, 100),
+                        length_bounds: Union[tuple, float] = (0, 2 ** 32),
+                        quality_threshold: int = 0) -> None:
     """
-    Launch filtering fastaq seq using 3 adjustable cutoffs. Allowed intervals include cutoffs values.
+    Launch filtering fastq seq using 3 adjustable cutoffs. Allowed intervals include cutoffs values.
 
-    :param input_path: path to input fastaq file
-    :param output_filename: name of output fastaq file
+    :param input_path: path to input fastq file
+    :param output_filename: name of output fastq file
     :param gc_bounds: cutoff for GC content in percents. You can specify lower and upper limits (tuple with floats)
     or just upper limit (then pass float). Default = (0,100)
     :param length_bounds: cutoff fot length in nucleic bases. You can specify lower and upper limits (tuple with floats)
