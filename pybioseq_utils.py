30d5b35b88137b3b98bab3aeddcbe1205dbec61c
Polina Vaganova <polvagser1@gmail.com>
2024-02-24 20:49:00 +0300

    Add code for nucleic acids analysis in OOP format.

--- pybioseq_utils.py
+++ pybioseq_utils.py
@@ -1,9 +1,98 @@
 import os
 from typing import Union
 from Bio import SeqIO, SeqUtils
+from abc import ABC, abstractmethod
 
 
-# function for fastaq files filtering
+class NucleotideNotFoundError(ValueError):
+    """
+    Error raised when there is incorrect nucleotide in corresponding sequence.
+    """
+    pass
+
+
+class BiologicalSequence(ABC):
+    @abstractmethod
+    def __len__(self):
+        """Return the length of the biological sequence."""
+        pass
+
+    def __getitem__(self, item):
+        """Return the element or slice of the biological sequence."""
+        pass
+
+    def __str__(self):
+        """Return the string representation of the biological sequence."""
+        pass
+
+    def is_correct(self):
+        """Check if the alphabet of the sequence is correct."""
+        pass
+
+
+class NucleicAcidSequence(BiologicalSequence, ABC):
+    def __init__(self, seq: str):
+        self.seq = seq
+        self.complement_pairs = {}
+
+    def is_correct(self):
+        """
+        Check if seq dna/rna or not
+        """
+        for nucleotide in set(self.seq):
+            if nucleotide.upper() not in self.complement_pairs.keys():
+                raise NucleotideNotFoundError(f'Sequence contains invalid nucleotide: {nucleotide}')
+        return None
+
+    def complement(self):
+        """
+        Create complementary nucleic seq
+        """
+        self.is_correct()
+        complement_seq = ''
+
+        for nucleotide in self.seq:
+            if nucleotide.isupper():
+                complement_seq += self.complement_pairs[nucleotide]
+            else:
+                complement_seq += self.complement_pairs[nucleotide.upper()].lower()
+        result = type(self)(complement_seq)
+        return result
+
+    def gc_content(self):
+        self.is_correct()
+        return SeqUtils.gc_fraction(self.seq)
+
+
+class DNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
+    def __init__(self, seq):
+        super().__init__(seq)
+        self.complement_pairs = {'A': 'T',
+                                 'T': 'A',
+                                 'C': 'G',
+                                 'G': 'C'}
+
+    def transcribe(self):
+        """
+        Calculate rna transcript from dna seq
+
+        :return:
+        - str: rna transcript
+        """
+        if 'T' in self.seq or 't' in self.seq:
+            return self.seq.replace('T', 'U').replace('t', 'u')
+        return self.seq
+
+
+class RNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
+    def __init__(self, seq):
+        super().__init__(seq)
+        self.complement_pairs = {'A': 'U',
+                                 'U': 'A',
+                                 'C': 'G',
+                                 'G': 'C'}
+
+
 def run_fastaq_filtering(input_path: str, output_filename: str = None, gc_bounds: Union[tuple, float, int] = (0, 100),
                          length_bounds: Union[tuple, float] = (0, 2 ** 32),
                          quality_threshold: int = 0) -> None:
@@ -54,3 +143,8 @@ def run_fastaq_filtering(input_path: str, output_filename: str = None, gc_bounds
     os.makedirs('fastq_filtrator_results', exist_ok=True)
 
     SeqIO.write(filtered_seqs, os.path.join('fastq_filtrator_results', output_filename), 'fastq')
+
+
+if __name__ == "__main__":
+    path_to_fasta = '/home/polina/bioinf/python_programming/HW6_Files/example_data/test.fastq'
+    run_fastaq_filtering(path_to_fasta, 'test.fastq', gc_bounds=(3, 50), quality_threshold=20)
