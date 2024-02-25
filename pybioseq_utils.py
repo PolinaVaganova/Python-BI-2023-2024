91be425f33f165821350615360e067383acdfaeb
Polina Vaganova <polvagser1@gmail.com>
2024-02-25 13:02:43 +0300

    Run linters: space changes.

--- pybioseq_utils.py
+++ pybioseq_utils.py
@@ -8,6 +8,7 @@ class NucleotideNotFoundError(ValueError):
     """
     Error raised when there is incorrect nucleotide in corresponding sequence.
     """
+
     pass
 
 
@@ -15,6 +16,7 @@ class AminoAcidNotFoundError(ValueError):
     """
     Error raised when there is incorrect aminoacid in corresponding sequence.
     """
+
     pass
 
 
@@ -76,15 +78,17 @@ class NucleicAcidSequence(BiologicalSequence, ABC):
         """
         for nucleotide in set(self.seq):
             if nucleotide.upper() not in self.complement_pairs.keys():
-                raise NucleotideNotFoundError(f'Sequence contains invalid nucleotide: {nucleotide}')
-        return 'Your sequence is correct.'
+                raise NucleotideNotFoundError(
+                    f"Sequence contains invalid nucleotide: {nucleotide}"
+                )
+        return "Your sequence is correct."
 
     def complement(self):
         """
         Create complementary nucleic acid sequence.
         """
         self.is_correct()
-        complement_seq = ''
+        complement_seq = ""
 
         for nucleotide in self.seq:
             if nucleotide.isupper():
@@ -99,7 +103,7 @@ class NucleicAcidSequence(BiologicalSequence, ABC):
         Calculate the GC content of the sequence as fraction (from 0 to 1).
         """
         self.is_correct()
-        return SeqUtils.gc_fraction(self.seq)
+        return SeqUtils.GC(self.seq)
 
 
 class RNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
@@ -115,10 +119,7 @@ class RNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
 
     def __init__(self, seq):
         super().__init__(seq)
-        self.complement_pairs = {'A': 'U',
-                                 'U': 'A',
-                                 'C': 'G',
-                                 'G': 'C'}
+        self.complement_pairs = {"A": "U", "U": "A", "C": "G", "G": "C"}
 
 
 class DNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
@@ -135,17 +136,14 @@ class DNASequence(NucleicAcidSequence, BiologicalSequence, ABC):
 
     def __init__(self, seq):
         super().__init__(seq)
-        self.complement_pairs = {'A': 'T',
-                                 'T': 'A',
-                                 'C': 'G',
-                                 'G': 'C'}
+        self.complement_pairs = {"A": "T", "T": "A", "C": "G", "G": "C"}
 
     def transcribe(self):
         """
         Calculate rna transcript from dna seq
         """
-        if 'T' in self.seq or 't' in self.seq:
-            transcript = type(self)(self.seq.replace('T', 'U').replace('t', 'u'))
+        if "T" in self.seq or "t" in self.seq:
+            transcript = type(self)(self.seq.replace("T", "U").replace("t", "u"))
         else:
             transcript = type(self)(self.seq)
 
@@ -171,39 +169,41 @@ class AminoAcidSequence(BiologicalSequence, ABC):
         count_aa(): Counts the occurrences of each amino acid in the sequence.
     """
 
-    def __init__(self, seq: str, code: int = 1
-                 ):
+    def __init__(self, seq: str, code: int = 1):
         self.code = code
-        self.residues_encoding = {'ALA': 'A',
-                                  'ARG': 'R',
-                                  'ASN': 'N',
-                                  'ASP': 'D',
-                                  'CYS': 'C',
-                                  'GLN': 'Q',
-                                  'GLU': 'E',
-                                  'GLY': 'G',
-                                  'HIS': 'H',
-                                  'ILE': 'I',
-                                  'LEU': 'L',
-                                  'LYS': 'K',
-                                  'MET': 'M',
-                                  'PHE': 'F',
-                                  'PRO': 'P',
-                                  'SER': 'S',
-                                  'THR': 'T',
-                                  'TRP': 'W',
-                                  'TYR': 'Y',
-                                  'VAL': 'V'
-                                  }
+        self.residues_encoding = {
+            "ALA": "A",
+            "ARG": "R",
+            "ASN": "N",
+            "ASP": "D",
+            "CYS": "C",
+            "GLN": "Q",
+            "GLU": "E",
+            "GLY": "G",
+            "HIS": "H",
+            "ILE": "I",
+            "LEU": "L",
+            "LYS": "K",
+            "MET": "M",
+            "PHE": "F",
+            "PRO": "P",
+            "SER": "S",
+            "THR": "T",
+            "TRP": "W",
+            "TYR": "Y",
+            "VAL": "V",
+        }
 
         if self.code == 1:
-            self.seq = [seq[val:val + 1] for val in range(0, len(seq), 1)]
+            self.seq = [seq[val : val + 1] for val in range(0, len(seq), 1)]
             self.residues_names = self.residues_encoding.values()
         elif self.code == 3:
-            self.seq = [seq[val:val + 3] for val in range(0, len(seq), 3)]
+            self.seq = [seq[val : val + 3] for val in range(0, len(seq), 3)]
             self.residues_names = self.residues_encoding.keys()
         else:
-            raise ValueError(f'Invalid specified encoding: {self.code}. Available values: 1, 3.')
+            raise ValueError(
+                f"Invalid specified encoding: {self.code}. Available values: 1, 3."
+            )
 
     def __len__(self):
         return len(self.seq)
@@ -212,7 +212,7 @@ class AminoAcidSequence(BiologicalSequence, ABC):
         return self.seq[index]
 
     def __str__(self):
-        return ''.join(self.seq)
+        return "".join(self.seq)
 
     def is_correct(self):
         """
@@ -220,8 +220,10 @@ class AminoAcidSequence(BiologicalSequence, ABC):
         """
         for residue in set(self.seq):
             if residue.upper() not in self.residues_names:
-                raise AminoAcidNotFoundError(f'Sequence contains invalid aminoacid: {residue}')
-        return 'Your sequence is correct.'
+                raise AminoAcidNotFoundError(
+                    f"Sequence contains invalid aminoacid: {residue}"
+                )
+        return "Your sequence is correct."
 
     def count_aa(self):
         """
@@ -235,9 +237,13 @@ class AminoAcidSequence(BiologicalSequence, ABC):
         return residue_count
 
 
-def run_fastq_filtering(input_path: str, output_filename: str = None, gc_bounds: Union[tuple, float, int] = (0, 100),
-                        length_bounds: Union[tuple, float] = (0, 2 ** 32),
-                        quality_threshold: int = 0) -> None:
+def run_fastq_filtering(
+    input_path: str,
+    output_filename: str = None,
+    gc_bounds: Union[tuple, float, int] = (0, 100),
+    length_bounds: Union[tuple, float] = (0, 2**32),
+    quality_threshold: int = 0,
+) -> None:
     """
     Launch filtering fastq seq using 3 adjustable cutoffs. Allowed intervals include cutoffs values.
 
@@ -256,32 +262,38 @@ def run_fastq_filtering(input_path: str, output_filename: str = None, gc_bounds:
     """
     filtered_seqs = []
 
-    for record in SeqIO.parse(input_path, 'fastq'):
+    for record in SeqIO.parse(input_path, "fastq"):
         # create dict for collect filtration steps results
         filter_result = {}
 
         # filter based on GC content
         gc_percent = SeqUtils.gc_fraction(record) * 100
         if isinstance(gc_bounds, tuple):
-            filter_result['gc_content'] = (gc_bounds[0] <= gc_percent <= gc_bounds[1])
+            filter_result["gc_content"] = gc_bounds[0] <= gc_percent <= gc_bounds[1]
         else:
-            filter_result['gc_content'] = (gc_percent <= gc_bounds)
+            filter_result["gc_content"] = gc_percent <= gc_bounds
 
         # filter based on length
         length = len(record.seq)
         if isinstance(length_bounds, tuple):
-            filter_result['length'] = (length_bounds[0] <= length <= length_bounds[1])
+            filter_result["length"] = length_bounds[0] <= length <= length_bounds[1]
         else:
-            filter_result['length'] = (length <= length_bounds)
+            filter_result["length"] = length <= length_bounds
 
         # filter based on quality
-        quality = sum(record.letter_annotations['phred_quality']) / len(record)
-        filter_result['quality'] = (quality >= quality_threshold)
+        quality = sum(record.letter_annotations["phred_quality"]) / len(record)
+        filter_result["quality"] = quality >= quality_threshold
 
         # total filtering: take structures that passed all cutoffs and write them to the output file
-        if filter_result['gc_content'] and filter_result['length'] and filter_result['quality']:
+        if (
+            filter_result["gc_content"]
+            and filter_result["length"]
+            and filter_result["quality"]
+        ):
             filtered_seqs.append(record)
 
-    os.makedirs('fastq_filtrator_results', exist_ok=True)
+    os.makedirs("fastq_filtrator_results", exist_ok=True)
 
-    SeqIO.write(filtered_seqs, os.path.join('fastq_filtrator_results', output_filename), 'fastq')
+    SeqIO.write(
+        filtered_seqs, os.path.join("fastq_filtrator_results", output_filename), "fastq"
+    )
