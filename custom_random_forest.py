2470cdb0628c77840b603e88c6b34e0131c6371a
Polina Vaganova <polvagser1@gmail.com>
2024-04-30 23:15:13 +0300

    chore: add annotation and code documentation

--- custom_random_forest.py
+++ custom_random_forest.py
@@ -1,18 +1,43 @@
 from multiprocessing import Pool
 
 import numpy as np
+from numpy import ndarray
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeClassifier
 
 
 class RandomForestClassifierCustom(BaseEstimator):
+    """
+    Custom Random Forest Classifier.
+
+    Parameters:
+        n_estimators (int): The number of trees in the forest.
+        max_depth (int): The maximum depth of the tree.
+        max_features (int): The number of features to consider when looking for the best split.
+        random_state (int): The seed used by the random number generator.
+
+    Attributes:
+        classes_ (ndarray): The classes labels.
+        n_estimators (int): The number of trees in the forest.
+        max_depth (int): The maximum depth of the tree.
+        max_features (int): The number of features to consider when looking for the best split.
+        random_state (int): The seed used by the random number generator.
+        trees (list): List of decision tree classifiers.
+        feat_ids_by_tree (list): List of feature indices used by each tree.
+
+    Methods:
+        fit(X: ndarray, y: ndarray, n_jobs: int = 1) -> None: Fits the model to the training data.
+        predict_proba(X: ndarray, n_jobs: int = 1) -> ndarray: Predicts class probabilities for input data.
+        predict(X: ndarray, n_jobs: int) -> ndarray: Predicts class labels for input data.
+    """
+
     def __init__(
         self,
-        n_estimators=10,
-        max_depth=None,
-        max_features=None,
-        random_state=111,
-    ):
+        n_estimators: int = 10,
+        max_depth: int = None,
+        max_features: int = None,
+        random_state: int = 111,
+    ) -> None:
         self.classes_ = None
         self.n_estimators = n_estimators
         self.max_depth = max_depth
@@ -22,7 +47,18 @@ class RandomForestClassifierCustom(BaseEstimator):
         self.trees = []
         self.feat_ids_by_tree = []
 
-    def fit(self, X, y, n_jobs=1):
+    def fit(self, X: ndarray, y: ndarray, n_jobs: int = 1) -> None:
+        """
+        Fits the model to the training data.
+
+        Parameters:
+            X (ndarray): The training input samples.
+            y (ndarray): The target values.
+            n_jobs (int): The number of jobs to run in parallel. Defaults to 1.
+
+        Returns:
+            None
+        """
         self.classes_ = sorted(np.unique(y))
 
         pool = Pool(processes=n_jobs)
@@ -60,8 +96,17 @@ class RandomForestClassifierCustom(BaseEstimator):
 
         self.trees = [res.get() for res in results]
 
-    def predict_proba(self, X, n_jobs=1):
+    def predict_proba(self, X: ndarray, n_jobs: int = 1) -> ndarray:
+        """
+        Predicts class probabilities for input data.
 
+        Parameters:
+            X (ndarray): The input samples.
+            n_jobs (int): The number of jobs to run in parallel. Defaults to 1.
+
+        Returns:
+            ndarray: The class probabilities.
+        """
         pool = Pool(processes=n_jobs)
         results = []
 
@@ -80,7 +125,17 @@ class RandomForestClassifierCustom(BaseEstimator):
         probabilities = [res.get() for res in results]
         return np.mean(probabilities, axis=0)
 
-    def predict(self, X, n_jobs):
+    def predict(self, X: ndarray, n_jobs: int) -> ndarray:
+        """
+        Predicts class labels for input data.
+
+        Parameters:
+            X (ndarray): The input samples.
+            n_jobs (int): The number of jobs to run in parallel.
+
+        Returns:
+            ndarray: The predicted class labels.
+        """
         probs = self.predict_proba(X, n_jobs=n_jobs)
         predictions = np.argmax(probs, axis=1)
 
